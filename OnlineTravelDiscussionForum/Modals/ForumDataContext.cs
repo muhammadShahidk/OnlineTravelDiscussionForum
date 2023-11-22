eff235d7a627be559a815b7d810dd7eca81ca3db
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-22 11:27:06 +0500

    Add project files.

---
+++ OnlineTravelDiscussionForum/Modals/ForumDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineTravelDiscussionForum.Modals
+{
+    public class ForumDataContext:DbContext
+    {
+        public ForumDataContext(DbContextOptions<ForumDataContext> options) : base(options) { }
+        
+       public DbSet<User> users { get; set; }
+    }
+}
---
+++ OnlineTravelDiscussionForum/Modals/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineTravelDiscussionForum.Modals
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
