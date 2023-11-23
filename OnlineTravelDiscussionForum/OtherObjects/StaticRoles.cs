5b8e186358d9c382ab42bb1c8f758f87d51fbc97
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-23 12:05:29 +0500

    Rols upadated

--- OnlineTravelDiscussionForum/OtherObjects/StaticRoles.cs
+++ OnlineTravelDiscussionForum/OtherObjects/StaticRoles.cs
@@ -2,7 +2,7 @@
 {
     public class StaticRoles
     {
-        public const string OWNER = "OWNER";
+        public const string MODERATOR = "MODERATOR";
         public const string ADMIN = "ADMIN";
         public const string USER = "USER";
 
