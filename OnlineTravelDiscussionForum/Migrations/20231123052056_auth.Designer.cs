012c24cfdc1e839f794e275b493c1cde1016ad95
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-23 12:05:09 +0500

    migrations intial

--- OnlineTravelDiscussionForum/Migrations/20231123052056_auth.Designer.cs
+++ OnlineTravelDiscussionForum/Migrations/20231123052056_auth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineTravelDiscussionForum.Modals;
 
@@ -11,9 +12,11 @@ using OnlineTravelDiscussionForum.Modals;
 namespace OnlineTravelDiscussionForum.Migrations
 {
     [DbContext(typeof(ForumDbContext))]
-    partial class ForumDataContextModelSnapshot : ModelSnapshot
+    [Migration("20231123052056_auth")]
+    partial class auth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
