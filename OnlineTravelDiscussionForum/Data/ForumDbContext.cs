4a1075cc43448404d2257dbe70459a0c86e5dee6
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-02 23:55:01 +0500

    removed user table form the database now likend all the tables with applications user

--- OnlineTravelDiscussionForum/Data/ForumDbContext.cs
+++ OnlineTravelDiscussionForum/Data/ForumDbContext.cs
@@ -7,9 +7,8 @@ namespace OnlineTravelDiscussionForum.Data
 {
     public class ForumDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options) { }
+        public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options) {   }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<ApprovalRequest> ApprovalRequests { get; set; }
