46b17ff91d4f15dbf266e5f4b62a07a123824718
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-02 23:55:35 +0500

    Posts req and responce dtos

--- OnlineTravelDiscussionForum/Dtos/PsotDto.cs
+++ OnlineTravelDiscussionForum/Dtos/PsotDto.cs
@@ -2,14 +2,16 @@
 {
     public class PostRequestDto
     {
-        public string Title { get; set; }
-        public string Content { get; set; }
+
+        public string? Title { get; set; }
+        public string? Content { get; set; }
 
     }
 
     public class PostResponseDto : PostRequestDto
     {
-        public int Id { get; set; }
-     
+        public int PostID { get; set; }
+        public string UserID { get; set; }
+
     }
 }
