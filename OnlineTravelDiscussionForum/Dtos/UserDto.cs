49863ad613184464dbaa8f3452017d19ed513163
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-02 23:55:48 +0500

    users req and responce dtos

---
+++ OnlineTravelDiscussionForum/Dtos/UserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineTravelDiscussionForum.Dtos
+{
+    public class UserRequestDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+
+    public class UserResponseDto:UserRequestDto
+    {
+        public int UserID { get; set; }
+    }
+}
