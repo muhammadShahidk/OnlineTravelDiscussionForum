0b95f4cdc26798dc8fb90b718181aad0a2347d5a
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-02 23:57:50 +0500

    user service intial

---
+++ OnlineTravelDiscussionForum/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using OnlineTravelDiscussionForum.Interfaces;
+using OnlineTravelDiscussionForum.Modals;
+using System.Security.Claims;
+
+namespace OnlineTravelDiscussionForum.Services
+{
+    public class UserService : IUserService
+    {
+        public Task GetCurrentUserId()
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        public Task UpdatePassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
