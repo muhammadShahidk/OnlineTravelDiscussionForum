ab29a1134e511747c5d20c387953e73c04d4ad58
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-04 00:09:52 +0500

    add approvalRequestdto

---
+++ OnlineTravelDiscussionForum/Dtos/ApprovalRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using OnlineTravelDiscussionForum.Modals;
+
+namespace OnlineTravelDiscussionForum.Dtos
+{
+    public class ApprovalRequestDto
+    {
+
+        public ApprovalStatus Status { get; set; }
+        public int RequestId { get; set; }
+
+    }
+
+    public class ApprovalResponseDto : ApprovalRequestDto
+    {
+        public DateTime DateCreated { get; set; } 
+        public DateTime? DateUpdated { get; set; }
+        public string UserID { get; set; }
+
+
+
+    }
+}
