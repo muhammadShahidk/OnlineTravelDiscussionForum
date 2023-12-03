ff3ab717aec0835ce9e586d97b1601bfc53b5e81
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-04 00:11:25 +0500

    relations are upadated

---
+++ OnlineTravelDiscussionForum/Modals/SensitiveKeyword.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineTravelDiscussionForum.Modals
+{
+    public class SensitiveKeyword
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Keyword { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; } = null;
+        public ApplicationUser User { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
