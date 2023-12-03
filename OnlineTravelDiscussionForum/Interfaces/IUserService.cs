48609bcf890e7408b1e535d4704fb1ea2b726a5e
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-04 00:10:15 +0500

    upadated i User interface with new methods

--- OnlineTravelDiscussionForum/Interfaces/IUserService.cs
+++ OnlineTravelDiscussionForum/Interfaces/IUserService.cs
@@ -1,8 +1,13 @@
-﻿namespace OnlineTravelDiscussionForum.Interfaces
+﻿using OnlineTravelDiscussionForum.Modals;
+
+namespace OnlineTravelDiscussionForum.Interfaces
 {
     public interface IUserService
     {
-        public Task  GetCurrentUserId();
+        public Task<string>  GetCurrentUserId();
+        public Task<ApplicationUser> GetCurrentUser();
         public Task UpdatePassword();
+        public string FilterSensitiveWords(string Content, List<SensitiveKeyword> sensitiveWords);
+        public List<string> GetUserRools();
     }
 }
